cb3ae435f232ee8286977031139ecfcd818c9c43
Peter De Herdt <peter.deherdt@softathome.com>
2021-07-17 19:27:42 +0000

    Makes it possible for provider to add or delete table rows

--- src/rbus.c
+++ src/rbus.c
@@ -1885,6 +1885,66 @@ rbusError_t rbus_close(rbusHandle_t handle)
     return errorcode;
 }
 
+rbusError_t rbusTable_registerRow(
+    rbusHandle_t handle,
+    char const* tableName,
+    char const* rowName,
+    char const* aliasName,
+    uint32_t instNum)
+{
+    rbusError_t rc = RBUS_ERROR_SUCCESS;
+    comp_info* ci = (comp_info*)handle;
+    
+    elementNode* tableInstance = retrieveElement(ci->elementRoot, rowName);
+    elementNode* tableRegElem = retrieveElement(ci->elementRoot, tableName);
+    elementNode* tableInstElem = retrieveInstanceElement(ci->elementRoot, tableName);
+
+    if (tableInstance) {
+        return RBUS_ERROR_SUCCESS;
+    }
+
+    if(tableRegElem && tableInstElem)
+    {
+        tableInstElem = tableInstElem->parent;
+        registerTableRow(handle, tableInstElem, tableName, aliasName, instNum);
+        return RBUS_ERROR_SUCCESS;
+    }
+
+    return RBUS_ERROR_INVALID_INPUT;
+}
+
+rbusError_t rbusTable_unregisterRow(
+    rbusHandle_t handle,
+    char const* rowName)
+{
+    rbusError_t rc = RBUS_ERROR_SUCCESS;
+    comp_info* ci = (comp_info*)handle;
+
+    /*get the element for the row */
+    elementNode* rowRegElem = retrieveElement(ci->elementRoot, rowName);
+    elementNode* rowInstElem = retrieveInstanceElement(ci->elementRoot, rowName);
+
+    if(rowRegElem && rowInstElem)
+    {
+        /*switch to the row's table */
+        elementNode* tableRegElem = rowRegElem->parent;
+        elementNode* tableInstElem = rowInstElem->parent;
+
+        if(tableRegElem && tableInstElem)
+        {
+            unregisterTableRow(handle, rowInstElem);
+            return RBUS_ERROR_SUCCESS;
+        }
+    }
+    else
+    {
+        // not found - so nothing to delete
+        return RBUS_ERROR_SUCCESS;
+    }
+
+    return RBUS_ERROR_INVALID_INPUT;
+}
+
 rbusError_t rbus_regDataElements(
     rbusHandle_t handle,
     int numDataElements,
